4114ff847aba98b123624c60456d0757a4a967b8
Justin Lane <justin.l.lane@gmail.com>
2020-04-22 21:41:41 -0600

    Boilerplated

---
+++ iis_copy.bat
@@ -0,0 +1,1 @@
+robocopy c:\users\jlane\developer\mobius-test c:\inetpub\wwwroot\mobius-test /MIR /XD .git
---
+++ mobius-test.js
@@ -0,0 +1,7 @@
+(function() {
+  var app = angular.module("mobius-test",[]);
+
+  app.controller("mtCtrl",["$scope",controller]);
+
+  function controller() {}
+})();
