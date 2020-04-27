ab5b6a81f9922f0d5cff9c1b7b0225899778a2ce
Justin Lane <justin.l.lane@gmail.com>
2020-04-26 21:14:06 -0600

    Better logs and to hit rolls

--- mobius-test.js
+++ mobius-test.js
@@ -40,7 +40,7 @@
 
     $ctrl.redExample = `{"name":"Red One","units": [{"name": "Red One 1","size": 6,"type": "starship","components": [{"name": "hull","crit": "unitBase","health": {"pool": 9,"priority": 1},"presence": {"magnitude": 6,"channel": 1}},{"name": "shield","crit": "shield","health": {"pool": 2,"priority": 2,"transfer": false},"energy": {"draw": 2}},{"name": "beam","crit": "battery","attack": {"volley": 7,"target": 350},"energy": {"draw": 7}},{"name": "stl","crit": "engine","effects": {"defense": 150}},{"name": "lrs","crit": "sensor","sensor": {"strength": 1,"channel": 1,"resolution": 1},"energy": {"draw": 1}},{"name": "reactor","crit": "powerPlant","energy": {"capacity": 72}}]},{"name": "Red One 2","size": 6,"type": "starship","components": [{"name": "hull","crit": "unitBase","health": {"pool": 9,"priority": 1},"presence": {"magnitude": 6,"channel": 1}},{"name": "shield","crit": "shield","health": {"pool": 2,"priority": 2,"transfer": false},"energy": {"draw": 2}},{"name": "beam","crit": "battery","attack": {"volley": 7,"target": 350},"energy": {"draw": 7}},{"name": "stl","crit": "engine","effects": {"defense": 150}},{"name": "lrs","crit": "sensor","sensor": {"strength": 1,"channel": 1,"resolution": 1},"energy": {"draw": 1}},{"name": "reactor","crit": "powerPlant","energy": {"capacity": 72}}]},{"name": "Red One 3","size": 6,"type": "starship","components": [{"name": "hull","crit": "unitBase","health": {"pool": 9,"priority": 1},"presence": {"magnitude": 6,"channel": 1}},{"name": "shield","crit": "shield","health": {"pool": 2,"priority": 2,"transfer": false},"energy": {"draw": 2}},{"name": "beam","crit": "battery","attack": {"volley": 7,"target": 350},"energy": {"draw": 7}},{"name": "stl","crit": "engine","effects": {"defense": 150}},{"name": "lrs","crit": "sensor","sensor": {"strength": 1,"channel": 1,"resolution": 1},"energy": {"draw": 1}},{"name": "reactor","crit": "powerPlant","energy": {"capacity": 72}}]}]}`;
 
-    $ctrl.blueExample = `{"name":"Blue Two","units":[{"name": "Blue Two 1","size": 6,"type": "starship","components": [{"name": "hull","crit": "unitBase","health": {"pool": 9,"priority": 1},"presence": {"magnitude": 6,"channel": 1}},{"name": "shield","crit": "shield","health": {"pool": 2,"priority": 2,"transfer": false},"energy": {"draw": 2}},{"name": "beam","crit": "battery","attack": {"volley": 7,"target": 350},"energy": {"draw": 7}},{"name": "beam","crit": "battery","attack": {"volley": 7,"target": 350},"energy": {"draw": 7}},{"name": "stl","crit": "engine","effects": {"defense": 150}},{"name": "lrs","crit": "sensor","sensor": {"strength": 1,"channel": 1,"resolution": 1},"energy": {"draw": 1}},{"name": "reactor","crit": "powerPlant","energy": {"capacity": 72}}]}]}`;
+    $ctrl.blueExample = `{"name":"Blue Two","units":[{"name": "Blue Two 1","size": 6,"type": "starship","components": [{"name": "hull","crit": "unitBase","health": {"pool": 9,"priority": 1},"presence": {"magnitude": 6,"channel": 1}},{"name": "shield","crit": "shield","health": {"pool": 2,"priority": 2,"transfer": false},"energy": {"draw": 2}},{"name": "beam 1","crit": "battery","attack": {"volley": 7,"target": 350},"energy": {"draw": 7}},{"name": "beam 2","crit": "battery","attack": {"volley": 7,"target": 350},"energy": {"draw": 7}},{"name": "stl","crit": "engine","effects": {"defense": 150}},{"name": "lrs","crit": "sensor","sensor": {"strength": 1,"channel": 1,"resolution": 1},"energy": {"draw": 1}},{"name": "reactor","crit": "powerPlant","energy": {"capacity": 72}}]}]}`;
 
     $ctrl.$onInit = function() {
       $ctrl.groups = {
@@ -98,9 +98,12 @@
       // Get a list of targets for the red team
       _.forEach(settings.groups.blue.units,function(unit) {
         $ctrl.output.push(log(`Adding unit ${unit.name} to target list`,"log-entry-action"));
-        unit.team = "blue";
-        targets.red.push(unit.name);
-        targets.master[unit.name] = unit;
+        initializeUnit({
+          unit: unit,
+          team: "blue",
+          targets: targets.red,
+          units: targets.master
+        });
       });
 
       settings.groups.blue.targets = targets.blue;
@@ -108,14 +111,38 @@
       // Get a list of targets for the blue team
       _.forEach(settings.groups.red.units,function(unit) {
         $ctrl.output.push(log(`Adding unit ${unit.name} to target list`,"log-entry-action"));
-        unit.team = "red";
-        targets.blue.push(unit.name);
-        targets.master[unit.name] = unit;
+        initializeUnit({
+          unit: unit,
+          team: "red",
+          targets: targets.blue,
+          units: targets.master
+        });
       });
 
       settings.groups.red.targets = targets.red;
 
       return targets;
+    }
+
+    /*
+     *  initializeUnit() - this function prepares a unit object for use by the
+     * combat engine.  THis includes additional linking between groups
+     * and target lists, setting defaults in components, etc.
+     */
+    function initializeUnit(data) {
+      var unit = data.unit;
+      var team = data.team;
+
+      unit.team = team;
+
+      data.targets.push(unit.name);
+      data.units[unit.name] = unit;
+
+      _.forEach(unit.components,function(c) {
+        if(_.has(c,'attack')) {
+          c.attack.target = _.isNumber(c.attack.target) ? c.attack.target : 0;
+        }
+      });
     }
 
     function setupCombatBoard(settings) {
@@ -143,7 +170,7 @@
         $ctrl.output.push(log(`Begin Turn ${count}`,"log-entry-important"));
 
         _.forEach(state.targets.master,function(unit) {
-          var msg = `Doing something for ${unit.name}`;
+          var msg = `Planning turn for ${unit.name}`;
           $ctrl.output.push(log(msg,"log-entry-purple"));
           state.log.push(log(msg,"log-entry-purple"));
           // Need to make this a permanent entry into a combat action log
@@ -158,7 +185,6 @@
             attacks.push({
               actor: unit.name,
               target: target,
-              attack: a.attack,
               action: a.name
             });
             var msg = log(`${unit.name} is targeting ${target}`,"log-entry-action")
@@ -170,9 +196,27 @@
 
           // Attack said target(s)
           _.forEach(attacks,function(a) {
-            var dmg = _.random(1,a.attack.volley,false);
+            // Roll for to hit and then damage
+            var hit = _.random(1,1000,false);
+            var def = _.random(1,1000,false);
+
+            var actor = state.targets.master[a.actor];
+            var target = state.targets.master[a.target];
+            var attack = _.filter(actor.components,['name',a.action])[0].attack;
+            console.info(attack);
+
+            var msg = log(`${a.actor} is attacking ${a.target} with ${a.action}`,"log-entry-green");
+            $ctrl.output.push(msg);
+            state.log.push(msg);
+
+            hit = hit + attack.target;
+            var msg = log(`${actor.name} rolled a hit roll of ${hit} (${attack.target})`);
+            $ctrl.output.push(msg);
+            state.log.push(msg);
+
+            var dmg = _.random(1,attack.volley,false);
             a.damage = dmg;
-            var msg = log(`${a.actor} is attacking ${a.target} with ${a.action} for ${dmg}`,"log-entry-green");
+            var msg = log(`${a.actor} did ${dmg} damage`,"log-entry-green");
             $ctrl.output.push(msg);
             state.log.push(msg);
           });
